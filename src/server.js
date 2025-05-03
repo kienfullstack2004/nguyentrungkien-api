71178c00c63717d82db50255f37a4da6d528f759
kienfullstack2004 <kiennguyenfullstack2004@gmail.com>
2025-05-03 14:50:36 +0700

    up

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const initRoutes = require("./routes/initRoutes.routes");
 const database = require("./config/connectDatabase");
-require("dotenv").config();
 
 app.use(express.urlencoded({
     extended: true
@@ -18,5 +18,5 @@ app.use(cors({
 database.connect();
 initRoutes(app);
 app.listen(PORT, () => {
-    console.log("App listening on port", PORT);
+    console.log("App listening on port 5000 ", PORT);
 })
