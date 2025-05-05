6692ecbe19aa71e257c2058786fc43b9ad24e511
kienfullstack2004 <kiennguyenfullstack2004@gmail.com>
2025-05-05 14:37:43 +0700

    update

--- src/server.js
+++ src/server.js
@@ -5,21 +5,21 @@ const cors = require("cors");
 const PORT = 5000;
 const initRoutes = require("./routes/initRoutes.routes");
 const database = require("./config/connectDatabase");
+const initAuth = require("./routes/auth.routes");
 database.connect();
 
 app.use(express.urlencoded({
     extended: true
 }))
+
 app.use(express.json())
 app.use(cors({
     origin: process.env.CLIENT,
     methods: ["DELETE", "POST", "PUT", "GET"]
-}
-))
-// initRoutes(app);
-app.get('/home',(req,res)=>{
-    res.send("Hello");
-})
+}))
+
+app.use('/',initAuth);
+
 const listening = app.listen(PORT, () => {
     console.log("App listening on port " + listening.address().port);
 })
